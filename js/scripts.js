4dbfec415b770134a275f24c5578e2dc4b892115
Kaveh Saleminejad and Kenny Wolfenberger <kaveh@>
2018-10-17 09:07:49 -0700

    all files added

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+$(document).ready(function() {
+  $("#formOne").submit(function(event) {
+    var person1Input = $("input#person1").val().toUpperCase();
+    $(".person1").text(person1Input);
+    $("#story").show();
+    event.preventDefault();
+  });
+});
